f8ff2a8ce88531f1697fad43468fef251628941a
Lovro Zelić <lovro.zelic@icloud.com>
2023-04-28 17:47:37 +0200

    Add default user for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Default user
+default_user = Rails.application.credentials.render[:default_user]
+password = Rails.application.credentials.render[:default_pass]
+confirm_password = Rails.application.credentials.render[:default_confirm_pass]
+
+user = User.where(email: default_user).first_or_initialize
+user.update!(
+  password: password,
+  password_confirmation: confirm_password
+)
